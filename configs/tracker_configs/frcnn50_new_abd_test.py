6c4281000766f12470f73c6b3290e510fb6196e7
zhanr110 <zhanr110@mcmaster.ca>
2021-06-21 17:19:38 -0400

    modify root folder

--- configs/tracker_configs/frcnn50_new_abd_test.py
+++ configs/tracker_configs/frcnn50_new_abd_test.py
@@ -16,7 +16,7 @@ root = {
         "selection_interval" : [0,10000],
 
         "source" : {
-            "base_folder" : "/media/philipp/philippkoehl_ssd/MTA_ext_short/test",
+            "base_folder" : "/u40/zhanr110/MTA_ext_short/test",
             "cam_ids" : [0,1,2,3,4,5]
         }
 
