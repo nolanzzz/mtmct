f275cab0b9a46255413c3cbe0342502b79d08bbb
nolanzzz <nolanwork131@gmail.com>
2021-06-29 23:10:02 -0400

    update data path

--- configs/tracker_configs/frcnn50_new_abd_train.py
+++ configs/tracker_configs/frcnn50_new_abd_train.py
@@ -16,7 +16,7 @@ root = {
         "selection_interval" : [0,10000],
 
         "source" : {
-            "base_folder" : "/u40/zhanr110/MTA_ext_short/test/train",
+            "base_folder" : "/u40/zhanr110/MTA_ext_short/train",
             "cam_ids" : [0,1,2,3,4,5]
         }
 
