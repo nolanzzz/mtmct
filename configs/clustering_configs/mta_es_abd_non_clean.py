34b9d3064e33f22faaa1128ca1eb09f50df76109
nolanzzz <nolanwork131@gmail.com>
2021-06-30 10:06:26 -0400

    modify multi track config

--- configs/clustering_configs/mta_es_abd_non_clean.py
+++ configs/clustering_configs/mta_es_abd_non_clean.py
@@ -2,11 +2,11 @@
 
 root = {
 
-        "work_dirs" : "/media/philipp/philippkoehl_ssd/work_dirs"
-        ,"train_track_results_folder" : "/home/philipp/Documents/repos/wda_tracker/work_dirs/tracker/config_runs/frcnn50_new_abd_train/tracker_results"
-        ,"test_track_results_folder" : "/home/philipp/Documents/repos/wda_tracker/work_dirs/tracker/config_runs/frcnn50_new_abd_test/tracker_results"
-        ,"train_dataset_folder" : "/media/philipp/philippkoehl_ssd/MTA_ext_short/train"
-        ,"test_dataset_folder" : "/media/philipp/philippkoehl_ssd/MTA_ext_short/test"
+        "work_dirs" : "/u40/zhanr110/wda_tracker/work_dirs"
+        ,"train_track_results_folder" : "/u40/zhanr110/wda_tracker/work_dirs/tracker/config_runs/frcnn50_new_abd_train/tracker_results"
+        ,"test_track_results_folder" : "/u40/zhanr110/wda_tracker/work_dirs/tracker/config_runs/frcnn50_new_abd_test/tracker_results"
+        ,"train_dataset_folder" : "/u40/zhanr110/wda_tracker/MTA_ext_short/train"
+        ,"test_dataset_folder" : "/u40/zhanr110/wda_tracker/MTA_ext_short/test"
         ,"cam_count" : 6
 
         ,"feature_extractor": {
