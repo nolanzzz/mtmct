95fdbc9ffc15ddaf4b1555ee05776d441782ba47
nolanzzz <nolanwork131@gmail.com>
2021-07-07 12:41:45 -0400

    update clustering config

--- configs/clustering_configs/mta_es_abd_non_clean.py
+++ configs/clustering_configs/mta_es_abd_non_clean.py
@@ -2,11 +2,16 @@
 
 root = {
 
-        "work_dirs" : "/u40/zhanr110/mtmct/work_dirs"
-        ,"train_track_results_folder" : "/u40/zhanr110/mtmct/work_dirs/tracker/config_runs/frcnn50_new_abd_train/tracker_results"
-        ,"test_track_results_folder" : "/u40/zhanr110/mtmct/work_dirs/tracker/config_runs/frcnn50_new_abd_test/tracker_results"
+        # "work_dirs" : "/u40/zhanr110/mtmct/work_dirs"
+        "work_dirs" : "/Users/nolanzhang/Projects/mtmct/work_dirs"
+        ,"train_track_results_folder" : "/u40/zhanr110/mtmct/work_dirs/tracker/config_runs/fair/tracker_results"
+        # ,"train_track_results_folder" : "/Users/nolanzhang/Projects/mtmct/work_dirs/tracker/config_runs/fair/tracker_results"
+        ,"test_track_results_folder" : "/u40/zhanr110/mtmct/work_dirs/tracker/config_runs/fair/tracker_results"
+        # ,"test_track_results_folder" : "/Users/nolanzhang/Projects/mtmct/work_dirs/tracker/config_runs/fair/tracker_results"
         ,"train_dataset_folder" : "/u40/zhanr110/MTA_ext_short/train"
+        # ,"train_dataset_folder" : "/Users/nolanzhang/Projects/mtmct/data/MTA_ext_short/train"
         ,"test_dataset_folder" : "/u40/zhanr110/MTA_ext_short/test"
+        # ,"test_dataset_folder" : "/Users/nolanzhang/Projects/mtmct/data/MTA_ext_short/test"
         ,"cam_count" : 6
 
         ,"feature_extractor": {
