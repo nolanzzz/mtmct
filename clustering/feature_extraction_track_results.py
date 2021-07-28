10e2c44acdac5d539e834bbbbd92c7a2e942b7e0
nolanzzz <nolanwork131@gmail.com>
2021-07-28 17:21:07 -0400

    testing

--- clustering/feature_extraction_track_results.py
+++ clustering/feature_extraction_track_results.py
@@ -48,7 +48,9 @@ class Feature_extraction:
 
             im_crops.append(im)
         print("im_crops len:", len(im_crops))
+        print("im_crops[0] len:", len(im_crops[0]))
         print("im_crops:", im_crops)
+        print("im_crops[0]:", im_crops[0])
         if im_crops:
             features = self.feature_extractor.extract(im_crops)
         else:
