5c2c7f270585156b99dca630021e09f25be01cbf
nolanzzz <nolanwork131@gmail.com>
2021-07-28 21:21:13 -0400

    print frame

--- clustering/feature_extraction_track_results.py
+++ clustering/feature_extraction_track_results.py
@@ -33,12 +33,12 @@ class Feature_extraction:
 
     def get_features(self, bboxes_xyxy, ori_img, debug=False):
         im_crops = []
+        print("get_features")
         for bbox in bboxes_xyxy:
             bbox = map(int,bbox)
             xtl, ytl, xbr, ybr = bbox
             im = ori_img[ytl:ybr, xtl:xbr]
-            print("im shape:", im.shape)
-            print("im:", im)
+            print("im shape:")
 
             if debug:
                 cv2.imshow('image', im)
