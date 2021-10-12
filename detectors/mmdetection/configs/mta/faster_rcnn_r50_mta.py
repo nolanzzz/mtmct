87d8c4f31868a3801ddf85ae87465359b5825dd4
nolanzzz <nolanwork131@gmail.com>
2021-10-12 00:42:59 -0400

    resume training 25e

--- detectors/mmdetection/configs/mta/faster_rcnn_r50_mta.py
+++ detectors/mmdetection/configs/mta/faster_rcnn_r50_mta.py
@@ -167,13 +167,13 @@ log_config = dict(
         # dict(type='TensorboardLoggerHook')
     ])
 # runtime settings
-total_epochs = 15
+total_epochs = 25
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/GtaDataset_12e'
+work_dir = './work_dirs/GtaDataset_15e'
 load_from = None
 # resume_from = None
-resume_from = './work_dirs/GtaDataset_12e/latest.pth'
+resume_from = './work_dirs/GtaDataset_15e/latest.pth'
 workflow = [('train', 1)]
 runner = dict(type='EpochBasedRunner', max_epochs=total_epochs)
 
