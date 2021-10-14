26c4209575e36425203e66ed027f04c790182547
nolanzzz <nolanwork131@gmail.com>
2021-10-13 21:59:12 -0400

    ready track script

--- track.sh
+++ track.sh
@@ -1,6 +1,4 @@
-cd ..
 # run tracker on train dataset first (cam 4,5 left)
 python run_tracker.py --config configs/tracker_configs/frcnn50_new_abd_train.py
 # then run tracker on test dataset (cam 3,4,5 left)
 python run_tracker.py --config configs/tracker_configs/frcnn50_new_abd_test.py
-cd experiments
