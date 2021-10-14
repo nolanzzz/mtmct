ef930ff7c4f289af074fef4b141b5be599ee1dc4
nolanzzz <nolanwork131@gmail.com>
2021-10-14 14:54:50 -0400

    update track script for 15e exp

--- track.sh
+++ track.sh
@@ -1,4 +1,4 @@
-# run tracker on train dataset first (cam 4,5 left)
-python run_tracker.py --config configs/tracker_configs/frcnn50_new_abd_train.py
-# then run tracker on test dataset (cam 3,4,5 left)
-python run_tracker.py --config configs/tracker_configs/frcnn50_new_abd_test.py
+# run tracker on test dataset (cam 4,5 left) with 15e model
+python run_tracker.py --config configs/tracker_configs/new_test_15e.py
+# run tracker on train dataset with 15e model
+python run_tracker.py --config configs/tracker_configs/new_test_15e_train.py
