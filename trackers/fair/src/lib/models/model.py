7bdb74571cf349ae577465ed36558dd4d12e5e9f
nolanzzz <nolanwork131@gmail.com>
2021-10-15 21:42:08 -0400

    rm debug

--- trackers/fair/src/lib/models/model.py
+++ trackers/fair/src/lib/models/model.py
@@ -34,8 +34,6 @@ def load_model(model, model_path, optimizer=None, resume=False,
                lr=None, lr_step=None):
   start_epoch = 0
   checkpoint = torch.load(model_path, map_location=lambda storage, loc: storage)
-  print(checkpoint.keys())
-  exit()
   print('loaded {}, epoch {}'.format(model_path, checkpoint['epoch']))
   state_dict_ = checkpoint['state_dict']
   state_dict = {}
