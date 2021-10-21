1c4785a48f6b250f764fa67210edddaafe595e9d
nolanzzz <nolanwork131@gmail.com>
2021-10-20 20:39:20 -0400

    debug cuda

--- track.sh
+++ track.sh
@@ -1,4 +1,4 @@
 # run tracker on test dataset (cam 4,5 left) with 15e model
 python run_tracker.py --config configs/tracker_configs/new_test_15e.py
 # run tracker on train dataset with 15e model
-python run_tracker.py --config configs/tracker_configs/new_test_30e.py
+CUDA_VISIBLE_DEVICES="1" python run_tracker.py --config configs/tracker_configs/new_test_30e.py
