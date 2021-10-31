f7741a1f5deb5110991fca447da5732cb3d0ce10
nolanzzz <nolanwork131@gmail.com>
2021-10-31 14:57:12 -0400

    sort cam_1

--- trackers/fair/utils/sort_gt.py
+++ trackers/fair/utils/sort_gt.py
@@ -4,7 +4,7 @@ import sys
 data_type = sys.argv[1]
 # cam #
 folder_no = sys.argv[2]
-directory = '../data/MTA/mta_data/images/' + data_type + '/cam_' + folder_no + '/gt'
+directory = '../data/MTA——short/mta_data/images/' + data_type + '/cam_' + folder_no + '/gt'
 lines = open(directory + '/unsorted.txt', 'r').readlines()
 output = open(directory + '/gt.txt', 'w')
 for line in sorted(lines, key=lambda l: int(l.split(',')[1]), reverse=False):
