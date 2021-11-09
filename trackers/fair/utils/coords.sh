f5bb224e12d7ae460fb2d4c2603f0945624c7430
nolanzzz <nolanwork131@gmail.com>
2021-11-08 21:53:14 -0500

    create coords shell to generate all cameras new coords

---
+++ trackers/fair/utils/coords.sh
@@ -0,0 +1,5 @@
+python modify_full_coords.py test 1
+python modify_full_coords.py test 2
+python modify_full_coords.py test 3
+python modify_full_coords.py test 4
+python modify_full_coords.py test 5
