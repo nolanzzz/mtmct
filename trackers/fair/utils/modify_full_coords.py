64590203159cb0124329a1bd8155152017eafcb6
nolanzzz <nolanwork131@gmail.com>
2021-11-08 21:39:53 -0500

    test with index

--- trackers/fair/utils/modify_full_coords.py
+++ trackers/fair/utils/modify_full_coords.py
@@ -4,18 +4,10 @@ import pandas as pd
 data_type = sys.argv[1]
 folder_no = sys.argv[2]
 
-# filename = '../data/MTA/mta_data/images/' + data_type + '/cam_' + folder_no + '/coords_cam_' + folder_no + '.csv'
 filename = '../data/MTA/MTA_videos_coords/' + data_type + '/cam_' + folder_no + '/coords_cam_' + folder_no + '.csv'
-# filename = '../data/MTA/mta_data/images/' + data_type + '/cam_' + folder_no + '/coords_result.csv'
 
 # file = np.loadtxt(filename, delimiter=',', skiprows=1)
-# output = '../data/MTA/mta_data/images/' + data_type + '/cam_' + folder_no + '/coords_result.csv'
-# output = '../data/MTA/mta_data/images/' + data_type + '/cam_' + folder_no + '/test.csv'
-output = '../data/MTA/MTA_videos_coords/' + data_type + '/cam_' + folder_no + '/test_' + folder_no + '.csv'
-
-# df = pd.read_csv(filename)
-# res = df.query('frame_no_cam >= 37843 & frame_no_cam <= 42763')
-# res = df.loc[(df['frame_no_cam'] >= 10) & (df['frame_no_cam'] <= 20)]
+output = '../data/MTA/MTA_videos_coords/' + data_type + '/cam_' + folder_no + '/test_index_' + folder_no + '.csv'
 
 chunksize = 10 ** 6
 chunk_no = 1
@@ -23,7 +15,7 @@ for chunk in pd.read_csv(filename, chunksize=chunksize):
     header = True if chunk_no == 1 else False
     df = chunk.query('frame_no_cam >= 37843 & frame_no_cam <= 42763')
     # df['frame_no_cam'] = df['frame_no_cam'].apply(lambda x: x - 37843)
-    df.to_csv(output, header=header, mode='a', index=False)
+    df.to_csv(output, header=header, mode='a')
     chunk_no += 1
 
 # res.to_csv(output)
