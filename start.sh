2dc5e27a9c95e5ac0494bd0adb5c11496abb4318
nolanzzz <nolanwork131@gmail.com>
2021-11-12 20:30:39 -0500

    create script to startup the whole procedure

---
+++ start.sh
@@ -0,0 +1,6 @@
+# one-shot script to run single then multi-cam tracking
+cd trackers/fair/src
+CUDA_VISIBLE_DEVICES="0" python track.py mot --test_mta True --exp_id "$1" --load_model '../exp/mot/high_30_complete/model_last.pth' --data_dir '../data/MTA_short/' --conf_thres 0.4 --gpus 0
+cd ../../..
+python run_tracker.py --config configs/tracker_configs/"$1".py
+python run_multi_cam_clustering.py --config configs/clustering_configs/"$1".py
