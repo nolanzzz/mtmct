997f3418c6bec476596bcba8abbcf9544b4ff983
nolanzzz <nolanwork131@gmail.com>
2021-11-15 12:17:26 -0500

    use gpu 3

--- run_multi_cam_tests.sh
+++ run_multi_cam_tests.sh
@@ -1,7 +1,7 @@
 # multi-cam clustering
 
 # 1 - WDA_Tracker - Cluster with WDA track results and features
-python run_multi_cam_clustering.py --config configs/clustering_configs/high_wda_20e.py
+CUDA_VISIBLE_DEVICES="3" python run_multi_cam_clustering.py --config configs/clustering_configs/high_wda_20e.py
 
 # 2 - Ours - Cluster with our single-camera track results and features
-python run_multi_cam_clustering.py --config configs/clustering_configs/fair_high_20e.py
+CUDA_VISIBLE_DEVICES="3" python run_multi_cam_clustering.py --config configs/clustering_configs/fair_high_20e.py
