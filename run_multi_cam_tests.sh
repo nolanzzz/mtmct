4147e72cb6390d2787f1689adc21e44d4c83aee7
nolanzzz <nolanwork131@gmail.com>
2021-11-15 11:54:26 -0500

    modify multi tests

--- run_multi_cam_tests.sh
+++ run_multi_cam_tests.sh
@@ -1,13 +1,7 @@
 # multi-cam clustering
 
-# clear cache before start
-rm -rf work_dirs/cache
+# 1 - WDA_Tracker - Cluster with WDA track results and features
+python run_multi_cam_clustering.py --config configs/clustering_configs/high_wda_20e.py
 
-# 1 - Ours - Cluster with our single-camera track results and features
+# 2 - Ours - Cluster with our single-camera track results and features
 python run_multi_cam_clustering.py --config configs/clustering_configs/fair_high_20e.py
-
-# clear cache before next run
-rm -rf work_dirs/cache
-
-# 2 - WDA_Tracker - Cluster with WDA track results and features
-CUDA_VISIBLE_DEVICES="2" python run_multi_cam_clustering.py --config configs/clustering_configs/high_wda_20e.py
