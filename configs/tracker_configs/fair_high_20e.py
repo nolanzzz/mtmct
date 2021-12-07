9b398e6bd80aae1b9e9889717917ef4a3e0f087b
nolanzzz <nolanwork131@gmail.com>
2021-12-07 13:38:51 -0500

    update config

--- configs/tracker_configs/fair_high_20e.py
+++ configs/tracker_configs/fair_high_20e.py
@@ -6,7 +6,7 @@ root = {
         #The visible GPUS will be restricted to the numbers listed here. The pytorch (cuda:0) numeration will start at 0
         #This is a trick to get everything onto the wanted gpus because just setting cuda:4 in the function calls will
         #not work for mmdetection. There will still be things on gpu cuda:0.
-        "cuda_visible_devices" : "2",
+        "cuda_visible_devices" : "0",
         "save_track_results" : True
 
     },
@@ -17,7 +17,6 @@ root = {
 
         "source" : {
             "base_folder" : "/u40/zhanr110/MTA_ext_short/test",
-            # "base_folder" : "/Users/nolanzhang/Projects/mtmct/data/MTA_ext_short/test",
             "cam_ids" : [0,1,2,3,4,5]
         }
 
